da33ced6908071a6c83e87c537d55f5ca47e0185
elebar15 <dev@higo.pm>
2025-04-29 21:31:06 +0000

    initial commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,135 @@
-import React from "react";
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark, faCheck } from '@fortawesome/free-solid-svg-icons';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+// create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [task, setTask] = useState({
+    task: "",
+    isDone: false
+  });
+  const [listTasks, setListTasks] = useState([]);
+  
+  // State to store hover state for each task
+  const [hoverState, setHoverState] = useState({});
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const handleEnter = (e) => {
+    setTask({
+      ...task,
+      task: e.target.value,
+    });
+  };
+
+  const saveTask = (e) => {
+    if (e.key === "Enter") {
+      setListTasks([
+        ...listTasks,
+        task
+      ]);
+      setTask({
+        task: "",
+        isDone: false
+      });
+    }
+  };
+
+  const deleteTask = (taskToDelete) => {
+    const newList = listTasks.filter((item) => item !== taskToDelete);
+    setListTasks(newList);
+  };
+
+  const taskDone = (taskBeDone) => {
+    const updatedList = listTasks.map((item) => {
+      if (item === taskBeDone) {
+        return {
+          ...item,
+          isDone: true
+        };
+      }
+      return item;
+    });
+    setListTasks(updatedList);
+  };
+
+  const handleMouseEnter = (index) => {
+    setHoverState((prev) => ({
+      ...prev,
+      [index]: true
+    }));
+  };
+
+  const handleMouseLeave = (index) => {
+    setHoverState((prev) => ({
+      ...prev,
+      [index]: false
+    }));
+  };
+
+  return (
+    <div className="container">
+      <p className="title mt-5 text-body-tertiary text-center">todos</p>
+      <div className="row">
+        <div className="col-8">
+          <div className="tasks">
+            <form onSubmit={(e) => e.preventDefault()}>
+              <input
+                type="text"
+                placeholder={listTasks !== "" ? "Add a new task" : "No task, add a task"}
+                name="task"
+                value={task.task}
+                onChange={handleEnter}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    saveTask(e);
+                  }
+                }}
+              />
+            </form>
+          </div>
+
+          {/* Task list */}
+          <div className="tasks">
+            <ul>
+              {listTasks.map((item, index) => {
+                return (
+                  <li
+                    className={`task text-body-tertiary ${item.isDone ? 'task-done' : ''}`}
+                    key={index}
+                    onMouseEnter={() => handleMouseEnter(index)}
+                    onMouseLeave={() => handleMouseLeave(index)}
+                  >
+                    {item.task}
+                    {hoverState[index] && (
+                      <span className="float-end">
+                        <FontAwesomeIcon
+                          icon={faXmark}
+                          className="icon delete me-2"
+                          onClick={() => deleteTask(item)}
+                        />
+                        <FontAwesomeIcon
+                          icon={faCheck}
+                          className={`icon ${item.isDone ? 'done' : ''}`}
+                          onClick={() => taskDone(item)}
+                        />
+                      </span>
+                    )}
+                    <hr />
+                  </li>
+                );
+              })}
+            </ul>
+            <hr />
+            <p className="items-number">
+              {(listTasks.length === 0) ? `${listTasks.length} item` : `${listTasks.length} items`}
+            </p>
+          </div>
+          {/* End of task list */}
+        </div>
+        <div className="subpaper1"></div>
+        <div className="subpaper2"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
